6adcbdf86ac6fd9075736aea26fb6acc0d8a4ffc
Arthur Khashaev <arthur@khashaev.ru>
2017-02-09 23:30:02 +0300

    Add transformation of LogicalExpression

--- packages/babel-plugin-transform-taintflow/src/visitor/index.ts
+++ packages/babel-plugin-transform-taintflow/src/visitor/index.ts
@@ -1,3 +1,4 @@
 export {MemberExpression} from "./MemberExpression";
 export {CallExpression} from "./CallExpression";
 export {BinaryExpression} from "./BinaryExpression";
+export {LogicalExpression} from "./LogicalExpression";
